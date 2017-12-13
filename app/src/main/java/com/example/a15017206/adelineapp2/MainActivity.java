baece5fe0468ad083e90d2696dc1d3202e48ca4c
15017206 <15017206@myrp.edu.sg>
2017-12-13 18:17:31 +0900

    1

--- app/src/main/java/com/example/a15017206/adelineapp2/MainActivity.java
+++ app/src/main/java/com/example/a15017206/adelineapp2/MainActivity.java
@@ -11,6 +11,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.SeekBar;
 
 import org.json.simple.parser.JSONParser;
 import org.json.JSONArray;
@@ -24,6 +25,13 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.Iterator;
 
+
+import android.widget.SeekBar;
+
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.TextView;
+
+
 public class MainActivity extends AppCompatActivity {
     String TAG = "";
 
@@ -33,8 +41,8 @@ public class MainActivity extends AppCompatActivity {
     ArrayList<SearchResult> searchResult = new ArrayList<>();
     ArrayAdapter aa;
     ImageView iv2;
-
-    String title, subtitle, price, shipping;
+    SeekBar seekBar;
+    TextView tv_seekbar_helper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,123 +52,148 @@ public class MainActivity extends AppCompatActivity {
         etSearch1 = findViewById(R.id.et_Search1);
         btnSearch1 = findViewById(R.id.btnSearch);
         customlv1 = findViewById(R.id.customlv1);
+        seekBar = findViewById(R.id.sbNoOfEntries);
+        tv_seekbar_helper = findViewById(R.id.tv_seekbarhelper);
 
-//        Picasso.with(this).load("http://st1.vchensubeswogfpjoq.netdna-cdn.com/wp-content/uploads/2014/11/Picasso-Image-Downloader.png").into(iv2);
+        seekBar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+tv_seekbar_helper.setText("Change to: " + i + " products");
+            }
 
-
-        // These're fake data
-//        searchResult.add(new SearchResult(R.drawable.star, "Title1", "Subtitle1", "$150.00", "Yes"));
-//        searchResult.add(new SearchResult(R.drawable.nostar, "Title2", "Subtitle2", "$250.00", "No"));
-
-        String appName = "?SECURITY-APPNAME=" + "GlennYeo-Software-PRD-35d705b3d-b15b0374";
-        String operationName = "&OPERATION-NAME=" + "findItemsByKeywords";
-        String serviceVersion = "&SERVICE-VERSION=" + "1.0.0";
-        String responseDataFormat = "&RESPONSE-DATA-FORMAT=" + "JSON";
-//        String callBack = "&callback=" + "_cb_findItemsByKeywords";
-        String callBack = "";
-        String keywords = "&keywords=" + "soap";
-        String paginationInputentriesPerPage = "&paginationInput.entriesPerPage=" + "13";
-        String paginationInputpageNumber = "&paginationInput.pageNumber=" + "1";
-        String globalid = "&GLOBAL-ID=" + "EBAY-US";
-
-        String totalParams = appName + operationName + serviceVersion + responseDataFormat + callBack + keywords + paginationInputentriesPerPage + paginationInputpageNumber + globalid;
-
-        //Some url endpoint that you may have
-        String myUrl = "https://svcs.ebay.com/services/search/FindingService/v1" + totalParams;
-
-        //String to place our result in
-        String result = "";
-
-        //Instantiate new instance of our class
-        HttpGetRequest getRequest = new HttpGetRequest();
-
-        //Perform the doInBackground method, passing in our url
-        try {
-            result = getRequest.execute(myUrl).get();
-            Log.i(TAG, "result is: " + result);
-
-            SearchResult searchResult2 = new SearchResult();
-
-            JSONObject jsonObject = new JSONObject(result);
-            JSONArray array_findItemsByKeywordsResponse = (JSONArray) jsonObject.get("findItemsByKeywordsResponse");
-
-            for (int i = 0; i < array_findItemsByKeywordsResponse.length(); i++) {
-                JSONObject jsonObject1 = array_findItemsByKeywordsResponse.getJSONObject(i);
-                Log.i(TAG, "inside findItemsBy: " + jsonObject1);
-
-                JSONArray array_searchResult = (JSONArray) jsonObject1.get("searchResult");
-
-                for (int j = 0; j < array_searchResult.length(); j++) {
-                    JSONObject jsonObject2 = array_searchResult.getJSONObject(j);
-                    Log.i(TAG, "inside searchResult: " + jsonObject2);
-
-
-                    JSONArray array_item = (JSONArray) jsonObject2.get("item");
-
-                    for (int k = 0; k < array_item.length(); k++) {
-                        JSONObject jsonObject3 = array_item.getJSONObject(k);
-                        Log.i(TAG, "inside item: " + jsonObject3);
-
-                        // Retrieve & Set text for Title
-                        JSONArray array_title = (JSONArray) jsonObject3.get("title");
-
-                        for (int l = 0; l < array_title.length(); l++) {
-                            String x = array_title.getString(i);
-                            searchResult2.setTvTitle(x);
-                            Log.i(TAG, "title is: " + x);
-                        }
-
-                        // SOME ENTRIES HAVE NO SUBTITLE- CHECK IF SUBTITLE KEY EXISTS!
-                        if (jsonObject3.has("subtitle")){
-                            // Retrieve & Set text for Subtitle
-                            JSONArray array_subtitle = (JSONArray) jsonObject3.get("subtitle");
-                            for (int l = 0; l < array_subtitle.length(); l++) {
-                                String x = array_subtitle.getString(i);
-                                searchResult2.setTvSubtitle(x);
-                            }
-                        }
-
-
-
-                        JSONArray array_sellingStatus = (JSONArray) jsonObject3.get("sellingStatus");
-                        for (int l = 0; l < array_sellingStatus.length(); l++) {
-
-                            JSONObject jsonObject4 = array_sellingStatus.getJSONObject(l);
-                            JSONArray array_currentPrice = (JSONArray) jsonObject4.get("currentPrice");
-
-                            // Set text for currency + value
-                            for (int m = 0; m < array_currentPrice.length(); m++) {
-                                JSONObject jsonObject5 = array_currentPrice.getJSONObject(m);
-                                String currencyID = jsonObject5.getString("@currencyId");
-                                String __value__ = jsonObject5.getString("__value__");
-
-                                searchResult2.setTvPrice(currencyID + " " + __value__);
-                            }
-                        }
-
-                        JSONArray array_galleryURL = (JSONArray) jsonObject3.get("galleryURL");
-                        for (int l = 0; l < array_galleryURL.length(); l++) {
-
-                            String galleryURL = array_galleryURL.getString(l);
-                            Log.i(TAG, "inside galleryURL: " + galleryURL);
-                            searchResult2.setImageView(galleryURL);
-                        }
-
-
-                        searchResult.add(searchResult2);
-
-                    }
-                }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
 
             }
 
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+btnSearch1.performClick();
+            }
+        });
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
 
-        aa = new SearchResultAdapter(this, R.layout.search_result_layout_row, searchResult);
-        customlv1.setAdapter(aa);
+        btnSearch1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                searchResult.clear();
+
+                // These're fake data
+//        searchResult.add(new SearchResult(R.drawable.star, "Title1", "Subtitle1", "$150.00", "Yes"));
+//        searchResult.add(new SearchResult(R.drawable.nostar, "Title2", "Subtitle2", "$250.00", "No"));
+
+                String appName = "?SECURITY-APPNAME=" + "GlennYeo-Software-PRD-35d705b3d-b15b0374";
+                String operationName = "&OPERATION-NAME=" + "findItemsByKeywords";
+                String serviceVersion = "&SERVICE-VERSION=" + "1.0.0";
+                String responseDataFormat = "&RESPONSE-DATA-FORMAT=" + "JSON";
+//        String callBack = "&callback=" + "_cb_findItemsByKeywords";
+                String callBack = "";
+                String keywords = "&keywords=" + etSearch1.getText().toString();
+                String paginationInputentriesPerPage = "&paginationInput.entriesPerPage=" + "13";
+                String paginationInputpageNumber = "&paginationInput.pageNumber=" + "1";
+                String globalid = "&GLOBAL-ID=" + "EBAY-US";
+
+                String totalParams = appName + operationName + serviceVersion + responseDataFormat + callBack + keywords + paginationInputentriesPerPage + paginationInputpageNumber + globalid;
+
+                //Some url endpoint that you may have
+                String myUrl = "https://svcs.ebay.com/services/search/FindingService/v1" + totalParams;
+
+                //String to place our result in
+                String result = "";
+
+                //Instantiate new instance of our class
+                HttpGetRequest getRequest = new HttpGetRequest();
+
+                //Perform the doInBackground method, passing in our url
+                try {
+                    result = getRequest.execute(myUrl).get();
+                    Log.i(TAG, "result is: " + result);
+
+                    SearchResult searchResult2 = new SearchResult();
+
+                    JSONObject jsonObject = new JSONObject(result);
+                    JSONArray array_findItemsByKeywordsResponse = (JSONArray) jsonObject.get("findItemsByKeywordsResponse");
+
+                    for (int i = 0; i < array_findItemsByKeywordsResponse.length(); i++) {
+                        JSONObject jsonObject1 = array_findItemsByKeywordsResponse.getJSONObject(i);
+                        Log.i(TAG, "inside findItemsBy: " + jsonObject1);
+
+                        JSONArray array_searchResult = (JSONArray) jsonObject1.get("searchResult");
+
+                        for (int j = 0; j < array_searchResult.length(); j++) {
+                            JSONObject jsonObject2 = array_searchResult.getJSONObject(j);
+                            Log.i(TAG, "inside searchResult: " + jsonObject2);
+
+
+                            JSONArray array_item = (JSONArray) jsonObject2.get("item");
+
+                            for (int k = 0; k < array_item.length(); k++) {
+                                JSONObject jsonObject3 = array_item.getJSONObject(k);
+                                Log.i(TAG, "inside item: " + jsonObject3);
+
+                                // Retrieve & Set text for Title
+                                JSONArray array_title = (JSONArray) jsonObject3.get("title");
+
+                                for (int l = 0; l < array_title.length(); l++) {
+                                    String x = array_title.getString(i);
+                                    searchResult2.setTvTitle(x);
+                                    Log.i(TAG, "title is: " + x);
+                                }
+
+                                // SOME ENTRIES HAVE NO SUBTITLE- CHECK IF SUBTITLE KEY EXISTS!
+                                if (jsonObject3.has("subtitle")) {
+                                    // Retrieve & Set text for Subtitle
+                                    JSONArray array_subtitle = (JSONArray) jsonObject3.get("subtitle");
+                                    for (int l = 0; l < array_subtitle.length(); l++) {
+                                        String x = array_subtitle.getString(i);
+                                        searchResult2.setTvSubtitle(x);
+                                    }
+                                }
+
+
+                                JSONArray array_sellingStatus = (JSONArray) jsonObject3.get("sellingStatus");
+                                for (int l = 0; l < array_sellingStatus.length(); l++) {
+
+                                    JSONObject jsonObject4 = array_sellingStatus.getJSONObject(l);
+                                    JSONArray array_currentPrice = (JSONArray) jsonObject4.get("currentPrice");
+
+                                    // Set text for currency + value
+                                    for (int m = 0; m < array_currentPrice.length(); m++) {
+                                        JSONObject jsonObject5 = array_currentPrice.getJSONObject(m);
+                                        String currencyID = jsonObject5.getString("@currencyId");
+                                        String __value__ = jsonObject5.getString("__value__");
+
+                                        searchResult2.setTvPrice(currencyID + " " + __value__);
+                                    }
+                                }
+
+                                JSONArray array_galleryURL = (JSONArray) jsonObject3.get("galleryURL");
+                                for (int l = 0; l < array_galleryURL.length(); l++) {
+
+                                    String galleryURL = array_galleryURL.getString(l);
+                                    Log.i(TAG, "inside galleryURL: " + galleryURL);
+                                    searchResult2.setImageView(galleryURL);
+                                }
+
+
+                                searchResult.add(searchResult2);
+
+                            }
+                        }
+
+                    }
+
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                aa = new SearchResultAdapter(MainActivity.this, R.layout.search_result_layout_row, searchResult);
+                customlv1.setAdapter(aa);
+            }
+        });
+
+
     }
 
     public class HttpGetRequest extends AsyncTask<String, Void, String> {
